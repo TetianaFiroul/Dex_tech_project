c7e8b5031e3950cc568d28c0aba34f80f588182d
TetianaBovkun <tetiana.bovkun@telecom-paristech.fr>
2020-05-07 18:16:12 +0200

    Datetime parse

--- js/dash_board.js
+++ js/dash_board.js
@@ -1,37 +1,18 @@
 let active_minutes_dataset=[];
-
-
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-
-// append the svg object to the body of the page
-var svg = d3.select("#distance")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S");
 
 
 
 
+d3.json("./data/lightly_active_minutes.json").then((data) => {
+       data.forEach((d) => {
+       d["dateTime"] = parseDate(d["dateTime"])
+       d["value"] = +d["value"]
+       });
+       console.log(data);
+       active_minutes_dataset = data
+
+   });
 
 
 
-
-
-
-d3.json("./data/lightly_active_minutes.json").then(function(d){
-        return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-      },
-
-
-    function(data) {
-    console.log(data);
-    active_minutes_dataset = data
-
-});
